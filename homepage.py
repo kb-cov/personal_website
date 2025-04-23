11240d238d7ad74e2271fe86ce269f6a0afb00c1
Kevin Banfield <kevin_b2000@hotmail.com>
2025-04-24 00:41:33 +0100

    Squashed commit of the following:
    commit 471e1c3f5a8b0a5821e1ff65302ac07869435371
    Author: Kevin Banfield <kevin_b2000@hotmail.com>
    Date:   Thu Apr 24 00:29:41 2025 +0100
    
        Remove print statements from get_crypto_prices.py
    
    commit 4768f47adcadb082b7a1ea8d60e2da187898337c
    Author: Kevin Banfield <kevin_b2000@hotmail.com>
    Date:   Wed Apr 23 23:43:18 2025 +0100
    
        Adds cryptocurrency data page and templates
    
        Introduces a new '/crypto' route to display cryptocurrency data using an external API. Updates the homepage to use a base template for consistency and enhances navigation styling.

--- homepage.py
+++ homepage.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
+from get_crypto_prices import endpoint_list, get_crypto_data
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/crypto")
+def crypto():
+    crypto_data = get_crypto_data(endpoint_list)
+    return render_template("crypto.html", crypto_data=crypto_data)
 
 
 if __name__ == "__main__":
