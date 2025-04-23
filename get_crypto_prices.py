4768f47adcadb082b7a1ea8d60e2da187898337c
Kevin Banfield <kevin_b2000@hotmail.com>
2025-04-23 23:43:18 +0100

    Adds cryptocurrency data page and templates
    Introduces a new '/crypto' route to display cryptocurrency data using an external API. Updates the homepage to use a base template for consistency and enhances navigation styling.

---
+++ get_crypto_prices.py
@@ -0,0 +1,35 @@
+import requests
+
+endpoint_list = [
+    "https://api.coinbase.com/v2/prices/BTC-USD/spot",
+    "https://api.coinbase.com/v2/prices/BTC-GBP/spot",
+    "https://api.coinbase.com/v2/prices/ETH-GBP/spot",
+    "https://api.coinbase.com/v2/prices/ADA-GBP/spot",
+    "https://api.coinbase.com/v2/prices/DOGE-GBP/spot",
+    "https://api.coinbase.com/v2/prices/XLM-GBP/spot",
+    "https://api.coinbase.com/v2/prices/NEAR-GBP/spot",
+    "https://api.coinbase.com/v2/prices/TRX-GBP/spot",
+    "https://api.coinbase.com/v2/prices/SHIB-GBP/spot",
+]
+
+def get_crypto_data(endpoint_list):
+    """
+    Fetches cryptocurrency prices from the Coinbase API and prints them.
+    The prices are formatted based on the currency type.
+    """
+    crypto_listings = []
+    for endpoint in endpoint_list:
+        response = requests.get(endpoint)
+        data = response.json()
+        crypto_listings.append(data)
+    return crypto_listings
+
+#print(get_crypto_data(endpoint_list))
+
+"""
+    for crypto in crypto_listings:
+        if crypto['data']['base'] != 'SHIB':
+            print(f"{crypto['data']['base']} : {float(crypto['data']['amount']):.4f} {crypto['data']['currency']}")
+        else:
+            print(f"{crypto['data']['base']} : {float(crypto['data']['amount']):.8f} {crypto['data']['currency']}")
+"""
